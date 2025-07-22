a023190c2d5f7d6748fde0c57e1cd9691517b857
Andrea Vassallo <andreavassallo@Andreas-MacBook-Air.local>
2025-07-22 09:58:29 +0200

    cambios de ayer

---
+++ src/iinstruments/bass.js
@@ -0,0 +1,12 @@
+
+  const button = document.getElementById("Button1");
+  const audio = document.getElementById("Audio");
+
+function soundbass(){
+    button.addEventListener("click", function() {
+        audio.play(); 
+    });
+    
+}
+
+  

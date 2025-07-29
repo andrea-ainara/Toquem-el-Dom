6cb00b66057971b1a7d14e92203651399a7bc0df
Andrea Vassallo <andreavassallo@Andreas-MacBook-Air.local>
2025-07-29 10:51:18 +0200

    finaaal

--- src/iinstruments/bass.js
+++ src/iinstruments/bass.js
@@ -4,11 +4,13 @@ const audio1 = document.getElementById("Audio1");
 function soundbass1() {
     button1.addEventListener("click", function () {
         audio1.play();
+          audio1.currentTime = 0;
     });
 
     document.addEventListener("keydown", (event) => {
         if (event.key === "5") {
             audio1.play();
+              audio1.currentTime = 0;
             button1.classList.remove('btnbass')
             button1.classList.add('noteActive')
         }
@@ -33,10 +35,12 @@ const audio2 = document.getElementById("audio2");
 function soundbass2() {
     button2.addEventListener("click", function () {
         audio2.play();
+          audio2.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "6") {
             audio2.play();
+              audio2.currentTime = 0;
             button2.classList.remove('btnbass')
             button2.classList.add('noteActive')
         }
@@ -60,10 +64,12 @@ const audio3 = document.getElementById("audio3");
 function soundbass3() {
     button3.addEventListener("click", function () {
         audio3.play();
+          audio3.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "7") {
            audio3.play();
+             audio3.currentTime = 0;
             button3.classList.remove('btnbass')
             button3.classList.add('noteActive')
         }
@@ -86,10 +92,12 @@ const audio4 = document.getElementById("audio4");
 function soundbass4() {
     button4.addEventListener("click", function () {
         audio4.play();
+          audio4.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "8") {
             audio4.play();
+              audio4.currentTime = 0;
             button4.classList.remove('btnbass')
             button4.classList.add('noteActive')
         }
@@ -112,10 +120,12 @@ const audio5 = document.getElementById("audio5");
 function soundbass5() {
     button5.addEventListener("click", function () {
         audio5.play();
+          audio5.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "r") {
              audio5.play();
+               audio5.currentTime = 0;
             button5.classList.remove('btnbass')
             button5.classList.add('noteActive')
         }
@@ -138,10 +148,12 @@ const audio6 = document.getElementById("audio6");
 function soundbass6() {
     button6.addEventListener("click", function () {
         audio6.play();
+          audio6.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "t") {
            audio6.play();
+             audio6.currentTime = 0;
             button6.classList.remove('btnbass')
             button6.classList.add('noteActive')
         }
@@ -164,10 +176,12 @@ const audio7 = document.getElementById("audio7");
 function soundbass7() {
     button7.addEventListener("click", function () {
         audio7.play();
+          audio7.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "y") {
          audio7.play();
+           audio7.currentTime = 0;
             button7.classList.remove('btnbass')
             button7.classList.add('noteActive')
         }
@@ -190,10 +204,12 @@ const audio8 = document.getElementById("audio8");
 function soundbass8() {
     button8.addEventListener("click", function () {
         audio8.play();
+          audio8.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "u") {
          audio8.play();
+           audio8.currentTime = 0;
             button8.classList.remove('btnbass')
             button8.classList.add('noteActive')
         }
@@ -215,10 +231,12 @@ const audio9 = document.getElementById("audio9");
 function soundbass9() {
     button9.addEventListener("click", function () {
         audio9.play();
+          audio9.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "d") {
           audio9.play();
+            audio9.currentTime = 0;
             button9.classList.remove('btnbass')
             button9.classList.add('noteActive')
         }
@@ -241,10 +259,12 @@ const audio10 = document.getElementById("audio10");
 function soundbass10() {
     button10.addEventListener("click", function () {
         audio10.play();
+          audio10.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "f") {
          audio10.play();
+           audio10.currentTime = 0;
             button10.classList.remove('btnbass')
             button10.classList.add('noteActive')
         }
@@ -267,10 +287,12 @@ const audio11 = document.getElementById("audio11");
 function soundbass11() {
     button11.addEventListener("click", function () {
         audio11.play();
+          audio11.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "g") {
       audio11.play();
+        audio11.currentTime = 0;
             button11.classList.remove('btnbass')
             button11.classList.add('noteActive')
         }
@@ -293,10 +315,12 @@ const audio12 = document.getElementById("audio12");
 function soundbass12() {
     button12.addEventListener("click", function () {
         audio12.play();
+          audio12.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "h") {
           audio12.play();
+            audio12.currentTime = 0;
             button12.classList.remove('btnbass')
             button12.classList.add('noteActive')
         }
@@ -319,10 +343,12 @@ const audio13 = document.getElementById("audio13");
 function soundbass13() {
     button13.addEventListener("click", function () {
         audio13.play();
+          audio13.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "x") {
         audio13.play();
+          audio13.currentTime = 0;
             button13.classList.remove('btnbass')
             button13.classList.add('noteActive')
         }
@@ -345,10 +371,12 @@ const audio14 = document.getElementById("audio14");
 function soundbass14() {
     button14.addEventListener("click", function () {
         audio14.play();
+          audio14.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "c") {
          audio14.play();
+           audio14.currentTime = 0;
             button14.classList.remove('btnbass')
             button14.classList.add('noteActive')
         }
@@ -370,10 +398,12 @@ const audio = document.getElementById("audio15");
 function soundbass15() {
     button15.addEventListener("click", function () {
         audio15.play();
+          audio15.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "v") {
           audio15.play();
+            audio15.currentTime = 0;
             button15.classList.remove('btnbass')
             button15.classList.add('noteActive')
         }
@@ -395,10 +425,12 @@ const audio16 = document.getElementById("audio16");
 function soundbass16() {
     button16.addEventListener("click", function () {
         audio16.play();
+          audio16.currentTime = 0;
     });
     document.addEventListener("keydown", (event) => {
         if (event.key === "b") {
           audio16.play();
+            audio16.currentTime = 0;
             button16.classList.remove('btnbass')
             button16.classList.add('noteActive')
         }
